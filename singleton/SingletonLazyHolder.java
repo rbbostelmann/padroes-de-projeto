8bfa051a0eec256affcaa953fb6ef93619f447e2
Rodrigo Bostelmann <50453044+rbbostelmann@users.noreply.github.com>
2022-07-12 14:07:53 -0300

    Update SingletonLazyHolder.java

--- singleton/SingletonLazyHolder.java
+++ singleton/SingletonLazyHolder.java
@@ -1,12 +1,5 @@
 package one.digitalinnovation.gof.singleton;
 
-/**
- * Singleton "Lazy Holder".
- * 
- * @see <a href="https://stackoverflow.com/a/24018148">Referencia</a>
- * 
- * @author falvojr
- */
 public class SingletonLazyHolder {
 
 	private static class InstanceHolder {
